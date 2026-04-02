e0b301b0288772420a9a1dff09215a0c98b47cb9
csuqiceng <csuqiceng@users.noreply.github.com>
2026-04-02 16:04:23 +0000

    feat: 默认不使用模拟控制器

--- ai_pipeline_prototype/gui.py
+++ ai_pipeline_prototype/gui.py
@@ -53,7 +53,7 @@ class PipelineAppUI:
         self.json_input_var = tk.StringVar(value='{"command": "MOVE", "parameters": {"target": "POSITION_1", "speed": 50}, "timestamp": "2026-04-02T12:00:00Z"}')
         
         # 控制器配置
-        self.force_mock_var = tk.BooleanVar(value=True)
+        self.force_mock_var = tk.BooleanVar(value=False)
         self.controller_host_var = tk.StringVar(value="127.0.0.1")
         self.controller_port_var = tk.StringVar(value="5000")
         self.connection_type_var = tk.StringVar(value="eth")
